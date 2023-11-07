825098de4abcb786f8c82fcfffcfa70a7497fc57
Duda <137435989+RainhaStealth@users.noreply.github.com>
2023-11-07 16:27:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let card = document.getElementById("card");
+
+// Exibir a mensagem caso seja selecionado o 'sim'
+document.querySelector("#yes").addEventListener("click", function () {
+  let question = document.getElementById("question");
+  let message = document.getElementById("answer");
+
+  card.style.backgroundImage =
+    "url('https://youtu.be/B8UsBmUjwTo?si=xuNBv1jUnDQmhwPD')";
+  question.style.display = "none";
+  message.style.display = "flex";
+});
+
+// Movimento aleatório do botão 'não'
+document.querySelector("#no").addEventListener("mouseover", function () {
+  let width = window.innerWidth - 50;
+  let height = window.innerHeight - 50;
+
+  this.style.position = "absolute";
+  this.style.top = Math.random() * width + "px";
+  this.style.left = Math.random() * height + "px";
+});
